cb841620ad5f9f1ebf2f442132277fc13ee03cc3
Vaidik26 <vaidiky90@gmail.com>
2025-06-19 20:45:32 +0530

    some changes in app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
 from src.logger import logging as lg
 import os, sys
-
-
 from src.pipeline.train_pipeline import TrainingPipeline
 from src.pipeline.predict_pipeline import PredictionPipeline
 
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+app = application
 
 
 @app.route("/")
